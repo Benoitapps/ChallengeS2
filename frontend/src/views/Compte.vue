2559f687e71504c636d706269325fbd854586cfe
Nicolas CAPELLA <colas.capel73@gmail.com>
2023-06-04 00:58:13 +0200

    create user account interface

--- frontend/src/views/Compte.vue
+++ frontend/src/views/Compte.vue
@@ -1,9 +1,28 @@
 <script setup>
+  import { ref } from 'vue'
 
+  const email = ref('')
+  const password = ref('')
+
+  const signup = () => {
+    fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value
+      })
+    })
+  }
 </script>
 
 <template>
   <h2>Compte</h2>
+  <input type="email" placeholder="email" v-model="email">
+  <input type="password" placeholder="password" v-model="password">
+  <button @click="signup">S'inscrire</button>
 </template>
 
 <style scoped lang="scss">
