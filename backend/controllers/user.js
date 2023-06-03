51865a32e923443d7df504ac4844cbfbcf86e9e9
Nicolas CAPELLA <colas.capel73@gmail.com>
2023-06-03 23:18:15 +0200

    setup server

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,42 +2,58 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-exports.signup = (req, res, next) => {
-    console.log(req.body)
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
+async function signup(req, res) {
+    try {
+        if (!req.body?.email || !req.body?.password) {
+            return res.status(400).json({ error: 'Missing parameters' });
+        }
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = new User({
             email: req.body.email,
-            password: hash
-            });
-            user.save()
-            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-            .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+            password: hashedPassword
+        });
+
+        await user.save();
+        res.status(201).json({ 
+            message: 'Utilisateur créé !',
+            email: user.email
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
-exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
-};
+async function login(req, res) {
+    try {
+        if (!req.body?.email || !req.body?.password) {
+            return res.status(400).json({ error: 'Missing parameters' });
+        }
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+        if (!validPassword) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+        }
+
+        const token = jwt.sign(
+            { userId: user._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' }
+        );
+
+        res.status(200).json({
+            userId: user._id,
+            token: token
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = { signup, login };
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
-const User = require('./models/User');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-require('dotenv').config({ path: '.env.local', override: true });
-
-const bdd = process.env.BDD;
+const { connect } = require('./services/mongoose');
 
 const app = express();
 
@@ -17,31 +10,9 @@ app.use(cors());
 // Use to parse JSON body
 app.use(express.json());
 
-mongoose.connect(bdd,
-  { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true 
-  }).then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.post('/signup', (req, res) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-              email: req.body.email,
-              password: hash
-            });
-            user.save()
-            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-            .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
-});
+app.use("/", userRoutes)
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
+  connect();
 });
