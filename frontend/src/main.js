3e77922c4e5003b5ce0b973400a1477f0ceacde8
Nicolas CAPELLA <colas.capel73@gmail.com>
2023-06-04 16:54:56 +0200

    add SDK to our frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue'
 import router from './router';
+import SDK from '../sdk';
 
 const app = createApp(App);
 app.use(router).mount('#app');
+
+new SDK();
