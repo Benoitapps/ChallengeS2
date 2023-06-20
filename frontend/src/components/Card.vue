6c7db114e19766a10f033b7c04ce51d5a66c58a8
MelvinCourant <melvin.courant1@gmail.com>
2023-06-20 14:04:43 +0200

    :sparkles: Start the feature of deleting cards

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -9,10 +9,6 @@ let props = defineProps({
     type: Number,
     default: 0
   },
-  favorite: {
-    type: Boolean,
-    default: false
-  },
   type: {
     type: String,
     default: "keys"
@@ -35,14 +31,13 @@ let props = defineProps({
   }
 });
 
-let isFavorite = ref(props.favorite);
+const emits = defineEmits(["removeCard"]);
+
+let isFavorite = ref(false);
 
 onMounted(() => {
-  if(localStorage.getItem(`favorite${+props.index}`) === "true") {
+  if(localStorage.getItem(props.title) !== null) {
     isFavorite.value = true;
-  }
-  else {
-    isFavorite.value = false;
   }
 
   handleLocalStorage();
@@ -50,17 +45,17 @@ onMounted(() => {
 
 function handleLocalStorage() {
   if(
-      isFavorite.value &&
-      localStorage.getItem(`favorite${+props.index}`) === null
+      isFavorite.value === true &&
+      localStorage.getItem(props.title) === null
   ) {
-    localStorage.setItem(`favorite${+props.index}`, true);
+    localStorage.setItem(props.title, "true");
   }
   else if(
-      !isFavorite.value &&
-      localStorage.getItem(`favorite${+props.index}`) !== null
+      isFavorite.value === false &&
+      localStorage.getItem(props.title) === "true"
   )
   {
-    localStorage.removeItem(`favorite${+props.index}`);
+    localStorage.removeItem(props.title);
   }
 }
 
@@ -69,42 +64,48 @@ function toggleFavorite() {
 
   handleLocalStorage();
 }
+
+function removeCard() {
+  emits("removeCard", props.index);
+}
 </script>
 
 <template>
-<div :class="['card', {favorite: isFavorite}]">
-  <div class="card__head">
-    <h2>{{ props.title }}</h2>
-    <More @toggleFavorite="toggleFavorite($event)"/>
-  </div>
-
-  <Keys
-      v-if="props.type === 'keys'"
-      :title="props.title"
-      :number="props.number"
-      :list="props.list"
-  />
-
-  <Chart
-      v-if="props.type === 'charts'"
-      :title="props.title"
-  />
-
   <div
-      class="card__footer"
-      v-if="props.periods.length > 0"
+      :class="['card', {favorite: isFavorite}]"
   >
-    <select class="card__footer__period">
-      <option
-          v-for="period in props.periods"
-          :key="period"
-          :value="period.value"
-      >
-        {{ period.label }}
-      </option>
-    </select>
+    <div class="card__head">
+      <h2>{{ props.title }}</h2>
+      <More @toggleFavorite="toggleFavorite($event)" @removeCard="removeCard($event)"/>
+    </div>
+
+    <Keys
+        v-if="props.type === 'keys'"
+        :title="props.title"
+        :number="props.number"
+        :list="props.list"
+    />
+
+    <Chart
+        v-if="props.type === 'charts'"
+        :title="props.title"
+    />
+
+    <div
+        class="card__footer"
+        v-if="props.periods.length > 0"
+    >
+      <select class="card__footer__period">
+        <option
+            v-for="period in props.periods"
+            :key="period"
+            :value="period.value"
+        >
+          {{ period.label }}
+        </option>
+      </select>
+    </div>
   </div>
-</div>
 </template>
 
 <style scoped lang="scss">
