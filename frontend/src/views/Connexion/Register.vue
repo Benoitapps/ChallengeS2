56f5c15eb6b10916b0892ee68a529c5a98b44e95
Benoit <benoitdecarli77@gmail.com>
2023-07-24 17:21:44 +0200

    ajout message erreur register

--- frontend/src/views/Connexion/Register.vue
+++ frontend/src/views/Connexion/Register.vue
@@ -30,7 +30,8 @@ export default {
         } else {
           // erreur
           const data = await response.json();
-          error.value = data.message;
+          error.value = data.error;
+          console.log(error.value)
         }
       } catch (e) {
         // Gestion des erreurs de connexion
