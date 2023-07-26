226c64402c6a90b8328920d4f519778aed2275d4
Alexandre Vauthier <alex.vauthier94@gmail.com>
2023-07-26 15:25:08 +0200

    :lipstick: style for api key

--- frontend/src/views/Profil/Profil.vue
+++ frontend/src/views/Profil/Profil.vue
@@ -126,18 +126,17 @@ getConnectedUser();
 
 
 <template>
-  <main  class="profile">
-    <div class="profile__container"></div>
-    <ul>
-      <li v-for="ligne in users" :key="ligne">
-        <p><span>Email:</span> {{ ligne.email }}</p>
-        <p><span>Site web:</span> {{ ligne.website }}</p>
-      </li>
-    </ul>
-    <p v-if="error" class="error">{{ error }}</p>
-    <router-link to="/profil/modifier" class="modifier-button">Modifier</router-link>
-
-    <div>
+  <main class="profile">
+    <div class="profile__container">
+      <h2>Mon profil</h2>
+      <ul>
+        <li v-for="ligne in users" :key="ligne">
+          <p><span>Email:</span> {{ ligne.email }}</p>
+          <p><span>Site web:</span> {{ ligne.website }}</p>
+        </li>
+      </ul>
+      <p v-if="error" class="error">{{ error }}</p>
+      <router-link to="/profil/modifier" class="modifier-button">Modifier</router-link>
       <h3>Votre clé API :</h3>
       <li v-for="ligne in users" :key="ligne">
         <div v-show="!showToken">
@@ -172,57 +171,65 @@ getConnectedUser();
 </template>
   
 <style scoped lang="scss">
-  .profile {
-    display: flex;
-    justify-content: center;
+.profile {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+
+  &__container {
+    max-width: 450px;
     width: 100%;
-    height: 100%;
+    height: fit-content;
+    background: var(--secondary);
+    padding: 2.5rem; // 40px
+    border-radius: 5px;
 
-    &__container {
-      max-width: 450px;
-      width: 100%;
-      height: fit-content;
-      background: var(--secondary);
-      padding: 2.5rem; // 40px
-      border-radius: 5px;
+    h2 {
+      text-align: center;
+      margin-bottom: 1.25rem;
+    }
 
-      h2 {
-        text-align: center;
-        margin-bottom: 1.25rem;
-      }
+    h3 {
+      text-align: center;
+      margin-bottom: 1.25rem;
+    }
 
-      li {
-        display: flex;
-        flex-direction: column;
-        gap: 0.5rem; // 8px
+    li {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem; // 8px
 
-        span {
-          font-weight: 500;
-        }
-      }
-
-      .error {
-        color: var(--error);
-      }
-
-      .modifier-button {
-        display: inline-block;
-        padding: 8px 16px;
-        background-color: var(--primary);
-        text-decoration: none;
-        border-radius: 4px;
-        text-align: center;
-        width: 100%;
-        margin-top: 1.25rem;
+      span {
+        font-weight: 500;
       }
     }
+
+    .error {
+      color: var(--error);
+    }
+
+    .modifier-button {
+      display: inline-block;
+      padding: 8px 16px;
+      background-color: var(--primary);
+      text-decoration: none;
+      border-radius: 4px;
+      text-align: center;
+      width: 100%;
+      margin-top: 1.25rem;
+    }
+
+    button {
+      display: inline-block;
+      padding: 8px 16px;
+      background-color: var(--primary);
+      text-decoration: none;
+      border-radius: 4px;
+      text-align: center;
+      width: 100%;
+      margin-top: 1.25rem;
+    }
   }
+}
 </style>
- 
-  
-  
-  
-  
-  
-  
-  
