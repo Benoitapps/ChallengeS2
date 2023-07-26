cbdb3ee040b1e51c0c1504bb9efa983bf2636832
Nicolas CAPELLA <colas.capel73@gmail.com>
2023-07-26 17:33:00 +0200

    :fire: add connect mongoose

--- backend/services/mongoose.js
+++ backend/services/mongoose.js
@@ -6,10 +6,10 @@ const BDD_URI = process.env.BDD;
 async function connect() {
     console.log('Connecting to MongoDB...')
     try {
-        // await mongoose.connect(BDD_URI, {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true
-        // });
+        await mongoose.connect(BDD_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error(error);
