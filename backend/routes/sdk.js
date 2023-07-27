3ecda54e2cb28f270161ceeb9926c4f282929390
Nicolas CAPELLA <colas.capel73@gmail.com>
2023-07-27 16:48:29 +0200

    message response send data

--- backend/routes/sdk.js
+++ backend/routes/sdk.js
@@ -66,10 +66,14 @@ router.post("/", async (req, res) => {
         await userTracker.save();
 
         // respond with data
-        res.status(200); // pas de json car on ne veut pas de réponse avec le sendBeacon
+        res.status(200).json({
+            message: 'Données sauvegardées !'
+        })
     } catch (error) {
         console.error(error);
-        res.status(500);
+        res.status(500).json({
+            message: "Echec de l'envoi des données"
+        });
     }
 });
 
